b5c50574a2edfeb5938e4930b7e7c98cc6de0f99
St3ve89 <istvan.acs89@gmail.com>
2018-06-05 00:56:25 +0100

    Add opacity to the click event and reset it when click on the other img

--- src/_asset/js/main.js
+++ src/_asset/js/main.js
@@ -1,5 +1,6 @@
 const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.4;
 
-imgs.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
+imgs.forEach(img => img.addEventListener('click', e => (imgs.forEach(img => (img.style.opacity = 1)), current.src = e.target.src, e.target.style.opacity = opacity)));
 
