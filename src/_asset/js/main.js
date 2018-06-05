18093083b97f3c59517a2277b41a70437775d527
St3ve89 <istvan.acs89@gmail.com>
2018-06-05 01:14:45 +0100

    Update 2018-06-05T00:09:44.730Z

--- src/_asset/js/main.js
+++
@@ -1,8 +0,0 @@
-const current = document.querySelector('#current');
-const imgs = document.querySelectorAll('.imgs img');
-const opacity = 0.6;
-
-imgs[0].style.opacity = opacity;
-
-imgs.forEach(img => img.addEventListener('click', e => (imgs.forEach(img => (img.style.opacity = 1)), current.src = e.target.src, current.classList.add('fade-in'), setTimeout(() => current.classList.remove('fade-in'), 500), e.target.style.opacity = opacity)));
-
