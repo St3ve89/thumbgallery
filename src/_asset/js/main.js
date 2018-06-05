70d06dfe9ce4a72cedd7f3e47e8dd5aa48bba6ce
St3ve89 <istvan.acs89@gmail.com>
2018-06-05 01:21:09 +0100

    refactoring js a bit

--- src/_asset/js/main.js
+++ src/_asset/js/main.js
@@ -4,5 +4,11 @@ const opacity = 0.6;
 
 imgs[0].style.opacity = opacity;
 
-imgs.forEach(img => img.addEventListener('click', e => (imgs.forEach(img => (img.style.opacity = 1)), current.src = e.target.src, current.classList.add('fade-in'), setTimeout(() => current.classList.remove('fade-in'), 500), e.target.style.opacity = opacity)));
+imgs.forEach(img => img.addEventListener('click',
+  e => (imgs.forEach(img => (img.style.opacity = 1)),
+  current.src = e.target.src, 
+  current.classList.add('fade-in'), 
+  setTimeout(() => current.classList.remove('fade-in'), 500), 
+  e.target.style.opacity = opacity
+)));
 
